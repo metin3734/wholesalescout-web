ff6faeea1b70d7dc6db5a87994da71c786cfe815
metin3734 <metin.tomar34@gmail.com>
2026-04-03 13:59:52 +0300

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-03-31.basil',
-  typescript: true,
+  apiVersion: '2026-02-25.clover',
 });
 
 // Plan → Stripe Price ID mapping
